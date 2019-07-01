9ceed920c74cbb306b1c5a0c494e800097fd2237
kondya <konds9975@gmail.com>
2019-07-01 20:06:28 +0530

    Initial Commit

---
+++ SmartServTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartServTest
+//
+//  Created by Kondya on 01/07/19.
+//  Copyright © 2019 Fortune4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
